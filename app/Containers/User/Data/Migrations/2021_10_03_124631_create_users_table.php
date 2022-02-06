fcb17d7e97c43b2231b1a78b31303e61c6130923
Stanislav Ermolaev <lumius.dev@gmail.com>
2022-02-06 19:29:33 +0300

    User update model and migration

--- app/Containers/User/Data/Migrations/2021_10_03_124631_create_users_table.php
+++ app/Containers/User/Data/Migrations/2021_10_03_124631_create_users_table.php
@@ -14,8 +14,19 @@ class CreateUsersTable extends Migration
     public function up(): void
     {
         Schema::create( 'users', function ( Blueprint $table ) {
-            $table->id();
+            $table->id()->unsigned()->autoIncrement();
             $table->timestamps();
+            $table->string( 'first_name' )->nullable();
+            $table->string( 'last_name' )->nullable();
+            $table->string( 'email' )->nullable();
+            $table->string( 'password' )->nullable();
+            $table->string( 'avatar' )->nullable();
+            $table->string( 'country' )->nullable();
+            $table->string( 'city' )->nullable();
+            $table->integer( 'count_photos' )->default( 0 );
+            $table->integer( 'count_reports' )->default( 0 );
+            $table->integer( 'count_likes' )->default( 0 );
+            $table->boolean( 'verified' )->default( 0 );
         } );
     }
 
--- app/Containers/User/Models/User.php
+++ app/Containers/User/Models/User.php
@@ -7,9 +7,29 @@ use App\Containers\User\Data\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * @property int $id
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $email
+ * @property string $password
+ * @property string $avatar
+ * @property int $count_photos
+ * @property int $count_reports
+ * @property int $count_likes
+ * @property int $verified
+ */
 class User extends Auth
 {
     use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+        'avatar'
+    ];
 
     protected static function newFactory(): UserFactory
     {
