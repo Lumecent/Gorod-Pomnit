7a0c0761b016fb643a5f87f3cbf0a8a9e6eacaf7
Stanislav Ermolaev <lumius.dev@gmail.com>
2022-02-09 13:38:31 +0300

    UserRepository implements UserRepositoryInterface

--- app/Containers/User/Repositories/UserRepository.php
+++ app/Containers/User/Repositories/UserRepository.php
@@ -3,9 +3,10 @@
 namespace App\Containers\User\Repositories;
 
 use App\Abstractions\Repositories\Repository as BaseRepository;
+use App\Containers\User\Interfaces\UserRepositoryInterface;
 use App\Containers\User\Models\User as Model;
 
-class UserRepository extends BaseRepository
+class UserRepository extends BaseRepository implements UserRepositoryInterface
 {
     protected static function getModelClass(): string
     {
