e603c52577b5a9bc354006c43264a22e2a1b9787
Stanislav Ermolaev <lumius.dev@gmail.com>
2022-02-10 12:54:22 +0300

    UserRepository aded methods create and updateById

--- app/Containers/User/Repositories/UserRepository.php
+++ app/Containers/User/Repositories/UserRepository.php
@@ -2,9 +2,12 @@
 
 namespace App\Containers\User\Repositories;
 
+use App\Abstractions\DTO\Dto;
 use App\Abstractions\Repositories\Repository as BaseRepository;
 use App\Containers\User\Interfaces\UserRepositoryInterface;
 use App\Containers\User\Models\User as Model;
+use App\Abstractions\Models\Model as BaseModel;
+use Illuminate\Support\Facades\Hash;
 
 class UserRepository extends BaseRepository implements UserRepositoryInterface
 {
@@ -12,4 +15,34 @@ class UserRepository extends BaseRepository implements UserRepositoryInterface
     {
         return Model::class;
     }
+
+    public function create( Dto $dto ): ?BaseModel
+    {
+        $user = $this->startConditions();
+
+        $user->email = $dto->email;
+        $user->password = Hash::make( $dto->password );
+
+        $user->save();
+
+        return $user;
+    }
+
+    public function updateById( Dto $dto ): ?BaseModel
+    {
+        if ( $user = $this->findById( $dto->id ) ) {
+            $user->first_name = $dto->first_name;
+            $user->last_name = $dto->last_name;
+            $user->country = $dto->country;
+            $user->city = $dto->city;
+            $user->avatar = $dto->avatar;
+
+            $user->save();
+
+            return $user;
+        }
+
+        return null;
+    }
+
 }
