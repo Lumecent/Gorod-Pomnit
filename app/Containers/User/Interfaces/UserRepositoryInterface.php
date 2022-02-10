219d45e5d4702005769db38aa6dda17b2b6eeaed
Stanislav Ermolaev <lumius.dev@gmail.com>
2022-02-10 17:12:46 +0300

    User added method findByEmail in UserRepositoryInterface

--- app/Containers/User/Interfaces/UserRepositoryInterface.php
+++ app/Containers/User/Interfaces/UserRepositoryInterface.php
@@ -7,4 +7,6 @@ use App\Abstractions\Models\Model;
 interface UserRepositoryInterface
 {
     public function findById( int $userId ): ?Model;
+
+    public function findByEmail( string $email ): ?Model;
 }
