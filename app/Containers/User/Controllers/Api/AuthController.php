c7f41381511381aa3aee2070a8e8602463b7efcc
Stanislav Ermolaev <lumius.dev@gmail.com>
2022-02-10 13:05:49 +0300

    User create AuthController

---
+++ app/Containers/User/Controllers/Api/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Containers\User\Controllers\Api;
+
+use App\Abstractions\Controllers\ApiController;
+use App\Abstractions\Responses\ApiResponse;
+use App\Containers\Token\Factories\TokenDtoFactory;
+use App\Containers\Token\Factories\TokenServiceFactory;
+use App\Containers\User\Factories\UserDtoFactory;
+use App\Containers\User\Factories\UserServiceFactory;
+use App\Containers\User\Requests\ApiRegisterRequest;
+use Illuminate\Support\Facades\DB;
+use Throwable;
+
+class AuthController extends ApiController
+{
+    public function registration( ApiRegisterRequest $request ): ApiResponse
+    {
+        DB::beginTransaction();
+
+        try {
+            $userDto = UserDtoFactory::getRegistrationDto()->fromRequest( $request );
+            $user = UserServiceFactory::getRegistrationService()->run( $userDto );
+
+            $tokenDto = TokenDtoFactory::getActivateAccountTokenDto()->fromArray( [
+                'user_id' => $user->id,
+                'token' => '',
+                'action' => ''
+            ] );
+            $token = TokenServiceFactory::getActivateAccountTokenService()->run( $tokenDto );
+
+            $activateDto = UserDtoFactory::getActivateAccountDto()->fromArray( [
+                'email' => $user->email,
+                'token' => $token->token
+            ] );
+            UserServiceFactory::getActivateAccountMessageService()->run( $activateDto );
+        } catch ( Throwable ) {
+            DB::rollBack();
+            return ApiResponse::sendData( 'Произошла ошибка! Повторите попытку позднее', [], 500 );
+        }
+
+        DB::commit();
+
+        return ApiResponse::sendData( 'На указанный e-mail было отправлено письмо для подтверждения регистрации' );
+    }
+}
